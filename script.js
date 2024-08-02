da4e4266703ce8fda956b3d4da6b8dc65297b82f
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2024-08-03 00:37:40 +0300

    new changes

---
+++ script.js
@@ -0,0 +1,44 @@
+   
+            // // Возрат в самое начало  
+            // // Получаем ссылку на кнопку
+            // const button = document.getElementById('main');
+            // // Добавляем обработчик события "click" на кнопку
+            // button.addEventListener('click', function() {
+            //     window.scrollTo({
+            //         top: 0,
+            //         behavior: 'smooth'
+            //     });
+            // });
+
+            
+            
+
+// Смена темы
+            // Получаем ссылку на кнопку
+            const themeToggle = document.getElementById('theme-change-button');
+            // Определяем функцию для смены темы
+            function toggleTheme() {
+                console.log(111)
+            // Получаем текущую тему из localStorage
+                let currentTheme = localStorage.getItem('theme');
+                console.log(currentTheme)
+                // Если текущая тема 'light', меняем её на 'dark'
+                if (currentTheme === 'light') {
+                    setTheme('dark');
+                } 
+                // Если текущая тема 'dark' или её нет, меняем на 'light'
+                else {
+                    setTheme('light');
+                }
+                
+            }
+            // Функция для установки темы
+            function setTheme(theme) {
+            // Сохраняем тему в localStorage
+            localStorage.setItem('theme', theme);
+            // Устанавливаем атрибут data-theme на корневом элементе
+            document.documentElement.setAttribute('data-theme', theme);
+            }
+
+            // Добавляем обработчик клика на кнопку
+            themeToggle.addEventListener('click', toggleTheme);
