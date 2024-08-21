16336cb1e1f0e4698a3e5b4141ab2c9e59f03523
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2024-08-21 15:30:50 +0300

    New changes

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
-   
-            // // Возрат в самое начало  
-            // // Получаем ссылку на кнопку
-            // const button = document.getElementById('main');
-            // // Добавляем обработчик события "click" на кнопку
-            // button.addEventListener('click', function() {
-            //     window.scrollTo({
-            //         top: 0,
-            //         behavior: 'smooth'
-            //     });
-            // });
+
+// // Возрат в самое начало  
+// // Получаем ссылку на кнопку
+// const button = document.getElementById('main');
+// // Добавляем обработчик события "click" на кнопку
+// button.addEventListener('click', function() {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// });
 
             
             
 
 // Смена темы
-            // Получаем ссылку на кнопку
-            const themeToggle = document.getElementById('theme-change-button');
-            // Определяем функцию для смены темы
-            function toggleTheme() {
-                console.log(111)
-            // Получаем текущую тему из localStorage
-                let currentTheme = localStorage.getItem('theme');
-                console.log(currentTheme)
-                // Если текущая тема 'light', меняем её на 'dark'
-                if (currentTheme === 'light') {
-                    setTheme('dark');
-                } 
-                // Если текущая тема 'dark' или её нет, меняем на 'light'
-                else {
-                    setTheme('light');
-                }
-                
-            }
-            // Функция для установки темы
-            function setTheme(theme) {
-            // Сохраняем тему в localStorage
-            localStorage.setItem('theme', theme);
-            // Устанавливаем атрибут data-theme на корневом элементе
-            document.documentElement.setAttribute('data-theme', theme);
-            }
 
-            // Добавляем обработчик клика на кнопку
-            themeToggle.addEventListener('click', toggleTheme);
+// Определяем функцию для смены темы
+function themeToggle() {
+// Получаем текущую тему из localStorage
+    let currentTheme = localStorage.getItem('theme');
+    console.log(currentTheme)
+    // Если текущая тема 'light', меняем её на 'dark'
+    if (currentTheme === 'light') {
+        setTheme('dark');
+    } 
+    // Если текущая тема 'dark' или её нет, меняем на 'light'
+    else {
+        setTheme('light');
+    }
+    
+}
+// Функция для установки темы
+function setTheme(theme) {
+// Сохраняем тему в localStorage
+localStorage.setItem('theme', theme);
+// Устанавливаем атрибут data-theme на корневом элементе
+document.documentElement.setAttribute('data-theme', theme);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    // Получаем ссылку на кнопку
+    let button = document.getElementById("buttonTheme");
+    button.addEventListener("click", themeToggle)
+  });
