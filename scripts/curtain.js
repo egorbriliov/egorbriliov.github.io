d070784e452eb50700c19541e3311303a30bff7c
Егор Брилёв <egor.briliov@gmai.com>
2024-08-21 20:44:23 +0300

    Changes

--- scripts/curtain.js
+++ scripts/curtain.js
@@ -1,18 +1,3 @@
-
-// // Возрат в самое начало  
-// // Получаем ссылку на кнопку
-// const button = document.getElementById('main');
-// // Добавляем обработчик события "click" на кнопку
-// button.addEventListener('click', function() {
-//     window.scrollTo({
-//         top: 0,
-//         behavior: 'smooth'
-//     });
-// });
-
-            
-            
-
 // Смена темы
 
 // Определяем функцию для смены темы
@@ -38,6 +23,7 @@ localStorage.setItem('theme', theme);
 document.documentElement.setAttribute('data-theme', theme);
 }
 
+// 
 window.addEventListener("DOMContentLoaded", () => {
     // Получаем ссылку на кнопку
     let button = document.getElementById("buttonTheme");
