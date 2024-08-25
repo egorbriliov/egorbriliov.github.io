707b56b97a3fde7be15fa1e3c1b9f22bae41709d
terminaate <tntwnik@gmail.com>
2024-08-25 13:28:31 +0300

    refactor some shit code

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,41 @@
-// Смена темы
-// Установка темы по умолчанию, как у пользователя в браузере
 function getCurrentTheme() {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      return 'dark';
-    } else {
-      return 'light';
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+        return storedTheme;
     }
-  }
+
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        return 'dark';
+    } else {
+        return 'light';
+    }
+}
 
 
-// Определяем функцию для смены темы
 function themeToggle() {
-// Получаем текущую тему из localStorage
-    let currentTheme = localStorage.getItem('theme');
-    // Если текущая тема 'light', меняем её на 'dark'
+    const currentTheme = localStorage.getItem('theme');
+
     if (currentTheme === 'light') {
         setTheme('dark');
-    } 
-    // Если текущая тема 'dark' или её нет, меняем на 'light'
-    else {
+    } else {
         setTheme('light');
     }
-
-    
 }
-// Функция для установки темы
+
 function setTheme(theme) {
-// Сохраняем тему в localStorage
-localStorage.setItem('theme', theme);
-// Устанавливаем атрибут data-theme на корневом элементе
-document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+
+    document.documentElement.setAttribute('data-theme', theme);
 }
 
-// 
 window.addEventListener("DOMContentLoaded", () => {
-    localStorage.setItem('theme', getCurrentTheme())
-    document.documentElement.setAttribute('data-theme', getCurrentTheme());
-    // Получаем ссылку на кнопку
-    let button = document.getElementById("buttonTheme");
-    button.addEventListener("click", themeToggle)
-  });
+    const currentTheme = getCurrentTheme()
+
+    localStorage.setItem('theme', currentTheme)
+
+    document.documentElement.setAttribute('data-theme', currentTheme);
+});
+
+const button = document.querySelector("#buttonTheme");
+
+button.addEventListener("click", themeToggle)
